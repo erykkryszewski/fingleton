6b198b6943d6f4241b4a2c5c45e28636bfbf6b15
Eryk Kryszewski <eric9972@o2.pl>
2025-12-12 09:46:34 +0100

    backend done

--- acf/blocks/two-columns-content.php
+++ acf/blocks/two-columns-content.php
@@ -1,7 +1,7 @@
 <?php 
 
 /**
- * ACF Block: Two columns text
+ * ACF Block: Two columns content
  *
  *
  * @package fingleton
@@ -12,11 +12,11 @@ $section_id = get_field('section_id');
 $background = get_field('background');
 $section_title = get_field('section_title');
 $left_column_text = get_field('left_column_text');
-$left_column_text_negative_margin = get_field('left_column_text_negative_margin');
-$left_column_text_button = get_field('left_column_text_button');
+$left_column_negative_margin = get_field('left_column_negative_margin');
+$left_column_button = get_field('left_column_button');
 $right_column_text = get_field('right_column_text');
-$right_column_text_negative_margin = get_field('right_column_text_negative_margin');
-$right_column_text_button = get_field('right_column_text_button');
+$right_column_negative_margin = get_field('right_column_negative_margin');
+$right_column_button = get_field('right_column_button');
 
 ?>
 
@@ -27,18 +27,18 @@ $right_column_text_button = get_field('right_column_text_button');
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-        <div class="two-columns-content__item two-columns-content__item--left <?php if('true' == $left_column_text_negative_margin) { echo 'two-columns-content__item--negative-margin'; }?>">
+        <div class="two-columns-content__item two-columns-content__item--left <?php if('true' == $left_column_negative_margin) { echo 'two-columns-content__item--negative-margin'; }?>">
             <?php echo apply_filters('the_title', $left_column_text);?>
-            <?php if(!empty($left_column_text_button)):?>
-                <a href="<?php echo esc_html($left_column_text_button['url']);?>" class="button two-columns-content__button"><?php echo esc_html($left_column_text_button['title']);?></a>
+            <?php if(!empty($left_column_button)):?>
+                <a href="<?php echo esc_html($left_column_button['url']);?>" class="button two-columns-content__button"><?php echo esc_html($left_column_button['title']);?></a>
             <?php endif;?>
         </div>
       </div>
       <div class="col-md-6">
-        <div class="two-columns-content__item two-columns-content__item--right <?php if('true' == $right_column_text_negative_margin) { echo 'two-columns-content__item--negative-margin'; }?>">
+        <div class="two-columns-content__item two-columns-content__item--right <?php if('true' == $right_column_negative_margin) { echo 'two-columns-content__item--negative-margin'; }?>">
             <?php echo apply_filters('the_title', $right_column_text);?>
-            <?php if(!empty($right_column_text_button)):?>
-                <a href="<?php echo esc_html($right_column_text_button['url']);?>" class="button two-columns-content__button"><?php echo esc_html($right_column_text_button['title']);?></a>
+            <?php if(!empty($right_column_button)):?>
+                <a href="<?php echo esc_html($right_column_button['url']);?>" class="button two-columns-content__button"><?php echo esc_html($right_column_button['title']);?></a>
             <?php endif;?>
         </div>
       </div>
